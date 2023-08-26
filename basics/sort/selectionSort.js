29c35bc5f52ae82000f040cf68153e8450eaa562
Ninad Bhoir <ninadbhoir010@gmail.com>
2023-08-26 11:38:02 +0530

    changes:selection sort

--- basics/sort/selectionSort.js
+++ basics/sort/selectionSort.js
@@ -1,18 +1,23 @@
 const selectinSort = (arr) => {
-  if (!Array.isArray(arr) || arr.length < 1) return arr;
+  if (!Array.isArray(arr) || arr.length <= 1) return arr;
+
   for (let i = 0; i < arr.length - 1; ++i) {
-    let minIndex = i; 
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; ++j) {
+      if (arr[j] < arr[minIndex]) {
         minIndex = j;
       }
     }
-    if (minIndex != i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+
+    if (minIndex !== i) {
+        const temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
   }
   return arr;
 };
 
-const call = selectinSort([40, 20, 70, 10, 30]);
-console.log(call);
+const arr = [20,12,53,3]
+console.log(selectinSort(arr));
