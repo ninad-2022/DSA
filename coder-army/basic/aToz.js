88c2abf3079699aac085843f463ee0305682a4ec
Ninad Bhoir <ninadbhoir010@gmail.com>
2023-10-31 06:13:17 +0530

    refractorig the code

--- coder-army/basic/aToz.js
+++
@@ -1,5 +0,0 @@
-for (let i = 65; i < 90; i++) {
-  const upperCase = String.fromCharCode(i);
-  const lowerCase = String.fromCharCode(i).toLowerCase();
-  console.log(upperCase + " - " + lowerCase);
-}
