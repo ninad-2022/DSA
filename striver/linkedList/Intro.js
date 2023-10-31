c59869b776c5fa14ba997013de5f6bc71d4655a5
Ninad Bhoir <ninadbhoir010@gmail.com>
2023-10-31 06:10:51 +0530

    linked list add and delete node

--- striver/linkedList/Intro.js
+++ striver/linkedList/Intro.js
@@ -1,5 +1,6 @@
 // https://www.codingninjas.com/studio/problems/introduction-to-linked-list_8144737?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
 
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
 class Node {
   constructor(value) {
     this.head = value;
@@ -22,8 +23,29 @@ class LinkedList {
     }
     return this.head;
   }
+
+  addHead(val) {
+    const newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  show() {
+    if (this.head !== null) return this.head;
+  }
+
+  deleteNode(node) {
+    // To delete the given node, we can simply copy the value of the next node to the current node
+    // and then update the current node's next pointer to skip the next node.
+    if (node === null || node.next === null) {
+      // Handle cases where the input node is invalid or it's the last node (which we cannot delete).
+      return;
+    }
+
+    node.value = node.next.value;
+    node.next = node.next.next;
+  }
+
 }
 
 const linkedList = new LinkedList();
-linkedList.constructLinkedList([4, 3, 5, 1]);
-// console.log("LinkedList: ",)
