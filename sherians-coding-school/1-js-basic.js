fcd7b35192d64feee302ae5b35d9545b3efc0e5a
ninad-2022 <ninad@quolam.com>
2025-02-26 16:17:23 +0530

    js basics

---
+++ sherians-coding-school/1-js-basic.js
@@ -0,0 +1,23 @@
+/*
+NOTE:
+    JS will check two operand between operators and then evaulte
+    String + Number = String
+    Number + String = Number
+*/
+
+let a = 10;
+let b = 20;
+
+console.log("sum of 10 and 20:- " + 10 + 20); //1020
+console.log("sum of 10 and 20:- " + (10 + 20)); //30
+console.log("sum of 10 and 20:- " + 30); //30
+
+console.log(a + b + " is sum of 10 and 20");
+console.log(typeof a + b + " is sum of 10 and 20");
+
+// Type Coersion: JS only does concatination with + but when it comes to -, *, /, + it converts string into number
+console.log("***********Type Coersion***********");
+console.log("1" + 1);
+console.log(2 - "1");
+console.log("1" * 2);
+console.log("1" / 2);
