1513f54d59101962644122a787acfcd12761b592
ninad-2022 <ninad@quolam.com>
2025-02-26 16:30:57 +0530

    develop: js basics

--- sherians-coding-school/1-js-basic.js
+++ sherians-coding-school/1-js-basic.js
@@ -15,8 +15,14 @@ console.log("sum of 10 and 20:- " + 30); //30
 console.log(a + b + " is sum of 10 and 20");
 console.log(typeof a + b + " is sum of 10 and 20");
 
-// Type Coersion: JS only does concatination with + but when it comes to -, *, /, + it converts string into number
-console.log("***********Type Coersion***********");
+/*
+
+Type Coersion: JS only does concatination with + but when it comes to -, *, /, + it converts string into number
+addition will only perform when both side has number else it will do concatination 
+
+*/
+
+console.log("*********** Type Coersion ***********");
 console.log("1" + 1);
 console.log(2 - "1");
 console.log("1" * 2);
